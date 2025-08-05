9ea55407a0d43ec6e5d749c1977150db21e7237c
Abdallh ELzayat <abdallhelzayat900@gmail.com>
2025-08-05 07:35:49 +0300

    Add API authentication with registration and login endpoints
    - Added `AuthController` with `register` and `login` methods for user authentication.
    - Created `RegisterRequest` and `LoginRequest` for validation of registration and login inputs.
    - Introduced `UserResource` to format user output in API responses.
    - Included Sanctum integration in the `User` model for token-based authentication.
    - Updated `api.php` routes to include `register` and `login` endpoints.

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Auth\LoginRequest;
+use App\Http\Requests\Api\Auth\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        if (!$user->save()) {
+            return response()->json(['message' => 'Registration failed'], 500);
+        }
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Registration successful',
+            'data' => [
+                'user' => UserResource::make($user),
+            ]
+        ], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $user = User::whereEmail($request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Login successful',
+            'data' => [
+                'user' => UserResource::make($user),
+                'token' => $token,
+            ]
+        ]);
+    }
+}
